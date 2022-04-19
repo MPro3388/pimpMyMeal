55e2086efde66f0c7f40e7ab6d117757ce74d790
markoprodanovic <s0543834@htw-berlin.de>
2022-04-19 15:07:51 +0200

    My Project

---
+++ src/main/java/de/pimpMyMeal/pimpMyMeal/PimpMyMealApplication.java
@@ -0,0 +1,13 @@
+package de.pimpMyMeal.pimpMyMeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimpMyMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimpMyMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/pimpMyMeal/pimpMyMeal/PimpMyMealApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pimpMyMeal.pimpMyMeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimpMyMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
